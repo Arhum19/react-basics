eae42b6d3437be99d424ab1407849e812a60cfd5
Arhum19 <arhumnoor10@gmail.com>
2025-09-02 10:57:48 +0500

    theme chagner added

--- theme-changer/src/App.jsx
+++ theme-changer/src/App.jsx
@@ -1,15 +1,28 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
 function App() {
-  const [theme, setTheme] = useState(false)
-  let toggle = () =>{
-    setTheme(!theme)
-  }
+  const [theme, setTheme] = useState(false);
+  let toggle = () => {
+    setTheme(!theme);
+  };
+  let style = {
+    backgroundColor: theme ? "black" : "white",
+    color: theme ? "white" : "black",
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  };
   return (
-    <div>
+    <div style={style}>
       <h1>{theme ? "DarkMode" : "LightMode"}</h1>
+      <p>This is basic theme changer</p>
+      <button onClick={toggle}>
+        Switch to {theme ? " light Mode" : "Dark Mode"}
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
