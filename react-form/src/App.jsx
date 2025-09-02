8ac5a9ee44b23776874e9e2046a771440439ee7f
Arhum19 <arhumnoor10@gmail.com>
2025-09-02 12:03:33 +0500

    form  added

--- react-form/src/App.jsx
+++ react-form/src/App.jsx
@@ -1,9 +1,18 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
   return (
-    <div>App</div>
-  )
+    <div>
+      <h1>Form Demo: </h1>
+      <form>
+        <label>Name: </label>
+        <input type="text"></input>
+        <br />
+        <button> Submit here:</button>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
